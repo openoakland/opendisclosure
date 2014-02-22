4e905be1a1973220639df5d9b55ddc0f89b8f657
Tom Dooner <tom.dooner@causes.com>
2014-02-22 12:31:39 -0800

    sqlite3 is out, postgres is in!

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ ruby '2.1.0'
 
 gem 'sinatra'
 gem 'activerecord'
+gem 'pg'
--- app.rb
+++ app.rb
@@ -1,6 +1,18 @@
 require 'sinatra'
 require 'active_record'
 
+# Load ActiveRecord models so we can query using an ORM
+Dir['./backend/models/*.rb'].each { |f| require f }
+
 get '/' do
-  return 'hello'
+  return "<a href='/individuals'>Individual Contributors</a>" +
+         "<a href='/others'>Other Contributors</a>"
+end
+
+get '/individuals' do
+  return '<ul>' + Individual.pluck(:name).join('</li><li>') + '</ul>'
+end
+
+get '/others' do
+  return '<ul>' + OtherContributor.pluck(:name).join('</li><li>') + '</ul>'
 end
