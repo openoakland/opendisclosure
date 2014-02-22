4d1b73b5c867fb59f06b6ec04cc1b15123806e0f
Tom Dooner <tom.dooner@causes.com>
2014-02-22 12:09:16 -0800

    Remove sqlite3 (heroku dislikes it)

--- Gemfile
+++ Gemfile
@@ -3,4 +3,3 @@ ruby '2.1.0'
 
 gem 'sinatra'
 gem 'activerecord'
-gem 'sqlite3'
--- app.rb
+++ app.rb
@@ -1,11 +1,6 @@
 require 'sinatra'
 require 'active_record'
 
-ActiveRecord::Base.establish_connection(
-  database: './backend/db.sqlite3',
-  adapter: 'sqlite3'
-)
-
 get '/' do
   return 'hello'
 end
