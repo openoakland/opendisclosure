6292bc8b6ba9be60043328eaa656b8b44c3d2145
Tom Dooner <tom.dooner@causes.com>
2014-03-11 20:42:12 -0700

    Fix Bryan Parker's name

--- backend/models/committee.rb
+++ backend/models/committee.rb
@@ -1,7 +1,7 @@
 class Committee < ActiveRecord::Base
   def self.mayoral_candidates
     {
-      find_by(committee_id: 1357609) => 'Brian Parker',
+      find_by(committee_id: 1357609) => 'Bryan Parker',
       find_by(committee_id: 1354678) => 'Jean Quan',
       find_by(committee_id: 1362261) => 'Libby Schaaf',
       find_by(committee_id: 1359017) => 'Joe Tuman',
