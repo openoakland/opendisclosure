30c086b938f2071f05218cb28c10d696a57b97c1
Ralph Santos <ralf@octagon.local>
2014-03-17 14:53:36 -0700

    Simple filter feature on recipients page

--- assets/js/app.js
+++ assets/js/app.js
@@ -115,4 +115,24 @@
       start();
     }
   });
+
+  // Adding this bit for the search feature on the contributors page
+  // Adding as a separate bit to make it easier to remove if something
+  // breaks due to its inclusion.
+  $(document).ready(function(){
+    $('#contributor').keyup(function() {
+      var filterval = $('#contributor').val().trim().toLowerCase();
+      $('li.contrib').each(function() {
+        var check_name = $(this).first('a').text().trim().toLowerCase();
+        if ( check_name.indexOf(filterval) >= 0 ) {
+          // $(this).css('background-color','cyan');
+          $(this).show();
+        } else {
+          // $(this).css('background-color','magenta');
+          $(this).hide();
+        }
+      });
+    });
+  });
+
 })();
