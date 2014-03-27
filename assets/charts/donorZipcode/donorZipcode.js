ef30efd89c1d715006f08e04e0fa918fd8ec4903
Vivian <vivian.brown@gmail.com>
2014-03-27 19:09:32 -0400

    Changed format of map files from geo-json to topo-json to reduce space

--- assets/charts/donorZipcode/donorZipcode.js
+++ assets/charts/donorZipcode/donorZipcode.js
@@ -1,4 +1,3 @@
-
 // Shapefiles from https://data.sfgov.org/
 // Converted to Geo-JSON using ogr2ogr
 // Geo-JSON simplified 50% - mapshaper.org
@@ -20,12 +19,18 @@
 				city = el.Tran_City;
 
 			// Add contributions by zip code
-			if (!amounts[zip]) { amounts[zip] = {}; }
-			if (!amounts[zip][candidate]) { amounts[zip][candidate] = 0; }
+			if (!amounts[zip]) {
+				amounts[zip] = {};
+			}
+			if (!amounts[zip][candidate]) {
+				amounts[zip][candidate] = 0;
+			}
 			amounts[zip][candidate] += amount;
 
 			// Create a list of all candidates
-			if (!candidates[candidate]) { candidates[candidate] = true; }
+			if (!candidates[candidate]) {
+				candidates[candidate] = true;
+			}
 		}
 		var candidates = _.keys(candidates);
 		// End of data processing
@@ -40,33 +45,34 @@
 			height = 800 - margin.top - margin.bottom;
 
 		var color = d3.scale.ordinal()
-  		.domain(candidates)
-  		.range(d3.range(12).map(function(i) { return "q" + i + "-12"; }));
+			.domain(candidates)
+			.range(d3.range(12).map(function(i) {
+				return "q" + i + "-12";
+			}));
 
 		var path = d3.geo.path()
 			.projection(d3.geo.albersUsa()
-				.scale(78000)
-				.translate([27400, 2800]));
+				.scale(90000)
+				.translate([31600, 3230]));
 
 		var svg = d3.select(chartEl).append("svg")
 			.attr("id", "map")
 			.attr("width", width + margin.right)
-      .attr("height", height);
-			
+			.attr("height", height);
+
 		var zipcodes = svg.append("g")
 			.attr("id", "bay-zipcodes");
 
 		var candidate = candidates[0];
 
-		d3.json("/charts/donorZipcode/bay_area_zip_codes.json", function(json) {
+		d3.json("/charts/donorZipcode/sfgov_bayarea_zipcodes_topo.json", function(json) {
+			data = topojson.feature(json, json.objects.layer1).features;
 
 			// Add map regions
 			var zips = zipcodes.selectAll("path")
-				.data(json.features)
+				.data(data)
 				.enter().append("svg:path")
-				.attr("id", function(d) {
-					zip = d.properties.ZIP;
-				})
+				.attr("id", function(d) { zip = d.properties.ZIP; })
 				.attr("d", path)
 				.attr('fill', '#d3d3d3')
 				.attr('stroke', '#9c9c9c')
@@ -79,8 +85,8 @@
 				.attr('id', 'circles');
 
 			radius = function(d) {
-    		if (amounts[d.properties.ZIP]){
-					return Math.sqrt( amounts[d.properties.ZIP][candidate] || 0)/10;
+				if (amounts[d.properties.ZIP]) {
+					return Math.sqrt(amounts[d.properties.ZIP][candidate] || 0) / 8;
 				}
 				return 0;
 			}
@@ -88,34 +94,42 @@
 			// Add a circle at the center of each zip
 			var dorling = circles.selectAll("circle")
 				.data(function() {
-					return json.features
+					return data
 				})
 				.enter()
 				.append("circle")
-				.each(function(d) { d.properties.c = path.centroid(d); })
-				.attr('cx', function(d) { return d.properties.c[0]; })
-				.attr('cy', function(d) { return d.properties.c[1]; })
+				.each(function(d) {
+					d.properties.c = path.centroid(d);
+				})
+				.attr('cx', function(d) {
+					return d.properties.c[0];
+				})
+				.attr('cy', function(d) {
+					return d.properties.c[1];
+				})
 				.attr('r', radius)
 				.attr('class', color(candidate));
 
 			// Update the chart when a user clicks on a candidate's name
-	    update = function() {
-	    	candidate = $(this).select('text').text();
-	    	dorling.attr('class', color(candidate))
-	    		.transition()
-	    		.attr('r', radius);
-	    }
+			update = function() {
+				candidate = $(this).select('text').text();
+				dorling.attr('class', color(candidate))
+					.transition()
+					.attr('r', radius);
+			}
 
-	    legend.on("click", update);
+			legend.on("click", update);
 		});
 
 		// Add outline for cities
-		d3.json("/charts/donorZipcode/bay_area_cities.json", function(json) {
+		d3.json("/charts/donorZipcode/sfgov_bayarea_cities_topo.json", function(json) {
+			data = topojson.feature(json, json.objects.layer1).features;
+
 			var cities = svg.append('g')
 				.attr('id', 'bay-cities');
 
 			var cities = cities.selectAll("path")
-				.data(json.features)
+				.data(data)
 				.enter().append("svg:path")
 				.attr("d", path)
 				.attr('fill', 'none')
@@ -124,35 +138,37 @@
 		});
 
 		// Add a legend at the bottom!
-  	var svg_legend = d3.select(chartEl).append("svg")
+		var svg_legend = d3.select(chartEl).append("svg")
 			.attr("id", "legend")
 			.attr("width", width + margin.right)
-      .attr("height", margin.bottom);
+			.attr("height", margin.bottom);
 
-    var offset = (width + margin.right)/candidates.length;
+		var offset = (width + margin.right) / candidates.length;
 
 		var legend = svg_legend.selectAll('.legend')
 			.data(candidates)
 			.enter().append('g')
 			.attr("class", "legend")
-      .attr("transform", function(d, i) {
-        return "translate(" + i * offset + ",0)";
-      });
+			.attr("transform", function(d, i) {
+				return "translate(" + i * offset + ",0)";
+			});
 
 		legend.append("rect")
-      .attr("x", 0)
-      .attr("width", offset)
-      .attr("height", 18)
-      .attr("class", function(d) {
-        return color(d);
-      });
+			.attr("x", 0)
+			.attr("width", offset)
+			.attr("height", 18)
+			.attr("class", function(d) {
+				return color(d);
+			});
 
-    legend.append("text")
-      .attr("x", offset/2)
-      .attr("y", 24)
-      .attr("dy", ".35em")
-      .style("text-anchor", "middle")
-      .text(function(d) { return d; });
+		legend.append("text")
+			.attr("x", offset / 2)
+			.attr("y", 24)
+			.attr("dy", ".35em")
+			.style("text-anchor", "middle")
+			.text(function(d) {
+				return d;
+			});
 
 	};
 
