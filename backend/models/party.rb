f34918602c1a2094759e796774068c8280e26fe8
Tom Dooner <tom.dooner@causes.com>
2014-04-01 19:46:17 -0700

    Fix bug with candidate names

--- backend/models/party.rb
+++ backend/models/party.rb
@@ -7,13 +7,15 @@ class Party < ActiveRecord::Base
     class_name: 'Contribution'
   has_many :summaries, primary_key: :committee_id
 
+  CANDIDATE_NAMES = {
+    1357609 => 'Bryan Parker',
+    1354678 => 'Jean Quan',
+    1362261 => 'Libby Schaaf',
+    1359017 => 'Joe Tuman',
+  }
+
   def self.mayoral_candidates
-    where(committee_id: [
-      1357609, # Bryan Parker
-      1354678, # Jean Quan
-      1362261, # Libby Schaaf
-      1359017, # Joe Tuman
-    ])
+    where(committee_id: CANDIDATE_NAMES.keys)
   end
 
   def latest_summary
