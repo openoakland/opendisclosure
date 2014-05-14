0beace28fe783b15edaed634c01309204bfef308
Tom Dooner + Vivian <tom.dooner+vivian.brown@causes.com>
2014-05-13 20:49:56 -0700

    Add percentage of contributions from Oakland

--- backend/models/contribution.rb
+++ backend/models/contribution.rb
@@ -1,4 +1,12 @@
 class Contribution < ActiveRecord::Base
   belongs_to :contributor, class_name: 'Party', counter_cache: :contributions_count
   belongs_to :recipient, class_name: 'Party', counter_cache: :received_contributions_count
+
+  after_create :increment_oakland_contribution_count
+
+  def increment_oakland_contribution_count
+    if contributor.from_oakland?
+      Party.increment_counter(:received_contributions_from_oakland, recipient.id)
+    end
+  end
 end
