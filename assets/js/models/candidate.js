acf3c98f02561b1c5358f9651d89a133cb0e5cc4
Michael Ubell <mike.ubell@gmail.com>
2014-07-02 09:10:21 -0700

    Move the "friendly" display routines to utils.js

--- assets/js/models/candidate.js
+++ assets/js/models/candidate.js
@@ -4,32 +4,24 @@ OpenDisclosure.Candidate = Backbone.Model.extend({
   },
 
   pctContributionsFromOakland : function() {
-    return this.friendlyPct(1 - this.attributes.received_contributions_from_oakland /
+    return OpenDisclosure.friendlyPct(1 - this.attributes.received_contributions_from_oakland /
                             this.attributes.received_contributions_count);
   },
 
   pctSmallContributions : function() {
-    return this.friendlyPct((
+    return OpenDisclosure.friendlyPct((
       this.attributes.small_donations +
          this.attributes.summary['total_unitemized_contributions']) /
       this.attributes.summary['total_contributions_received']);
   },
 
   avgContribution : function () {
-    return this.friendlyNumber(
+    return OpenDisclosure.friendlyMoney(
       this.attributes.summary['total_contributions_received']/
       this.attributes.received_contributions_count);
   },
 
   friendlySummaryNumber : function(which) {
-    return this.friendlyNumber(this.attributes.summary[which]);
+    return OpenDisclosure.friendlyMoney(this.attributes.summary[which]);
   },
-
-  friendlyNumber : function(number) {
-    return accounting.formatMoney(number);
-  },
-
-  friendlyPct : function(float) {
-    return Math.round(float * 10000) / 100 + "%";
-  }
 });
