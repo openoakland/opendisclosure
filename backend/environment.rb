d087b7929f41095243377a544954068f20888246
Tom Dooner <tom.dooner@causes.com>
2014-07-27 13:23:42 -0700

    Break out fetching logic into individual classes
    This may not prove to work.

--- backend/environment.rb
+++ backend/environment.rb
@@ -8,3 +8,4 @@ Dotenv.load
 ActiveRecord::Base.establish_connection ENV['DATABASE_URL']
 
 Dir[File.dirname(__FILE__) + '/models/*.rb'].each { |f| require f }
+Dir[File.dirname(__FILE__) + '/fetchers/*.rb'].each { |f| require f }
