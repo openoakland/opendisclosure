6157401910e914fba286a1349bfc3733eb587b8c
Tom Dooner <tom.dooner@causes.com>
2014-07-27 18:52:53 -0700

    Add initial test coverage
    This is a bit more verbose than I'd like, but it's a good starting
    point. We should be able to use FactoryGirl to manage creation of test
    objects either from the API or from the database, but how exactly that
    works I think will change over time.
    
    Closes #30

---
+++ spec/app_spec.rb
@@ -0,0 +1,18 @@
+require_relative '../app.rb'
+require 'rack/test'
+
+RSpec.configure do |c|
+  c.include Rack::Test::Methods
+end
+
+describe OpenDisclosureApp do
+  let(:app) { described_class }
+
+  describe '/' do
+    subject { get '/' }
+
+    # Just add a smoke test for now. Later, we can add some more meat to this
+    # test suite.
+    it { should be_ok }
+  end
+end
