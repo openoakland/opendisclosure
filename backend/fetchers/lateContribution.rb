89615c44464e97abed51673d9eda4108538c67cd
Michael Ubell <mike.ubell@gmail.com>
2014-09-11 15:00:06 -0700

    Split LateContribution fether to its own file.

--- backend/fetchers/lateContribution.rb
+++ backend/fetchers/lateContribution.rb
@@ -1,5 +1,5 @@
 class DataFetcher
-  class Contribution
+  class LateContribution
     def self.fetch_and_parse(url)
       SocrataFetcher.new(url).each do |record|
         parse_contributions(record)
@@ -16,31 +16,31 @@ class DataFetcher
           # contributor is a Committee and Cmte_ID is set. Same thing as
           # Filer_ID but some names disagree
           Party::Committee.where(committee_id: row['cmte_id'])
-                          .first_or_create(name: row['tran_naml'])
+                          .first_or_create(name: row['enty_naml'])
 
         when 'IND'
           # contributor is an Individual
-          full_name = row.values_at('tran_namt', 'tran_namf', 'tran_naml', 'tran_nams')
+          full_name = row.values_at('enty_namt', 'enty_namf', 'enty_naml', 'enty_nams')
                          .join(' ')
                          .strip
           Party::Individual.where(name: full_name,
-                                  city: row['tran_city'],
-                                  state: row['tran_state'],
-                                  zip: row['tran_zip4'])
+                                  city: row['enty_city'],
+                                  state: row['enty_st'],
+                                  zip: row['enty_zip4'])
                            .first_or_initialize
-                           .tap { |p| p.update_attributes(employer: row['tran_emp'], occupation: row['tran_occ']) }
+                           .tap { |p| p.update_attributes(employer: row['ctrib_emp'], occupation: row['ctrib_occ']) }
         when 'OTH'
           # contributor is "Other"
-          Party::Other.where(name: row['tran_naml'])
+          Party::Other.where(name: row['enty_naml'])
                       .first_or_initialize
-                      .tap { |p| p.update_attributes(city: row['tran_city'], state: row['tran_state'], zip: row['tran_zip4']) }
+                      .tap { |p| p.update_attributes(city: row['enty_city'], state: row['enty_st'], zip: row['enty_zip4']) }
         end
 
       ::Contribution.create(
         recipient: recipient,
         contributor: contributor,
-        amount: row['tran_amt1'],
-        date: row['tran_date'],
+        amount: row['amount'],
+        date: row['ctrib_date'],
         type: 'contribution'
       )
     end
