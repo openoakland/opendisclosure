c4ccd51afd914f818c196ef83a23d14fd2a462b2
Vivian <vivian.brown@gmail.com>
2014-09-25 22:39:45 -0700

    Remove contribution date from 'contributors to all candidates in this election' table. No data is rendering and it doesn't make sense.

--- assets/js/views/_contributorsView.js
+++ assets/js/views/_contributorsView.js
@@ -12,7 +12,6 @@ OpenDisclosure.ContributorsView = Backbone.View.extend({
         <a href="<%= contribution.contributorLinkPath() %>">\
           <span class="col-xs-6"><%= contribution.attributes.contributor.name %></span>\
           <span class="col-xs-2"><%= OpenDisclosure.friendlyMoney(contribution.attributes.amount) %><%= contribution.typeName() %></span>\
-          <span class="col-xs-4"><%= contribution.attributes.date ? moment(contribution.attributes.date).format("MMM-DD-YY"): "" %></span>\
         </a>\
       </div>'),
 
