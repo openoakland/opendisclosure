45bf8293090b57047f1620a549574c1f2791b446
Tom Dooner <tom.dooner@causes.com>
2014-10-09 22:02:26 -0700

    Remove four column topContributors view
    Not sure if it was a merge conflict or something, but the `col-sm-6`
    makes there be 4 columns of contributions instead of 2.

--- assets/js/views/_topContributorsView.js
+++ assets/js/views/_topContributorsView.js
@@ -1,9 +1,9 @@
 OpenDisclosure.TopContributorsView = Backbone.View.extend({
   template: _.template(' \
-    <div class="col-sm-6 col-xs-12">\
+    <div class="col-sm-12">\
       <a href="<%= employerLinkPath() %>">\
-      	<span class="col-xs-8"><%= attributes.contrib %></span>\
-      	<span class="col-xs-4"><%= OpenDisclosure.friendlyMoney(attributes.amount) %> </span>\
+        <span class="col-xs-8"><%= attributes.contrib %></span>\
+        <span class="col-xs-4"><%= OpenDisclosure.friendlyMoney(attributes.amount) %> </span>\
       </a>\
     </div>'),
 
@@ -15,23 +15,28 @@ OpenDisclosure.TopContributorsView = Backbone.View.extend({
   render: function() {
     this.$el.empty();
     $('<h2>Top Contributors to ' + this.options.candidate + '</h2>\
-      <h4>employees grouped with their employer</h4>\
-      <div class="topContributions clearfix">\
-        <div class="leftCol col-sm-6"></div>\
-        <div class="rightCol col-sm-6"></div>\
+        <h4>employees grouped with their employer</h4>\
+        <div class="topContributions clearfix">\
+        <div class="col-sm-6 leftCol"></div>\
+        <div class="col-sm-6 rightCol"></div>\
       </div>')
     .appendTo(this.$el);
 
-    var that = this;
     var half  = Math.round(this.collection.length/2);
     var left  = this.collection.slice(0,half);
     var right = this.collection.slice(half);
-    _.each(left, function(c){
-      that.$el.find('.topContributions .leftCol').append(that.template(c));
-    });
-    _.each(right, function(c){
-      that.$el.find('.topContributions .rightCol').append(that.template(c));
-    });
+
+    this.$el.find('.leftCol').html(
+      _.map(left, function(c) {
+        return this.template(c);
+      }.bind(this))
+    );
+
+    this.$el.find('.rightCol').html(
+      _.map(right, function(c) {
+        return this.template(c);
+      }.bind(this))
+    );
 
     this.$el.append("<h5 class='footnote'>For more Details on how we group businesses and employers see the <a href='/faq#groupBy'>FAQ</a></h5>");
   },
