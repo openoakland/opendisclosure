916dc94c39cd09c9c091fe713f64ace1b01fca6f
Michael Ubell <mike.ubell@gmail.com>
2014-10-15 15:37:12 -0700

    avoid loading data with missing amount and contributor

--- backend/fetchers/lateContribution.rb
+++ backend/fetchers/lateContribution.rb
@@ -7,6 +7,10 @@ class DataFetcher
     end
 
     def self.parse_contributions(row)
+      if (row['amount'].nil? and row['enty_naml'].nil? and row['enty_namf'].nil?) then
+	puts "Skipping " + row.values_at('filer_naml', 'tran_id').join(':');
+	return
+      end
       recipient = if row['filer_id'] == 0    # "pending"
                     Party::Committee.where(committee_id: 0, name: row['filer_naml'])
                                     .first_or_create
